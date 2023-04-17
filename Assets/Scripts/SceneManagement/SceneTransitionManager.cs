b109cc330305938f78d9659306e213a4b915b66b
Jonas Mortensen <jonasmortensen3@gmail.com>
2023-04-17 15:18:11 +0200

    Removed unused using directives

--- Assets/Scripts/SceneManagement/SceneTransitionManager.cs
+++ Assets/Scripts/SceneManagement/SceneTransitionManager.cs
@@ -1,10 +1,5 @@
-using System;
 using System.Collections.Generic;
-using Unity.Mathematics;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.Android;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
