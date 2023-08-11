bd2d4f9dcdcdfa5301a3b1882d973466fc2712d4
RemyUnity <remym@unity3d.com>
2023-08-11 19:11:06 +0200

    Add start benchmar bindings with new input system ?

--- Assets/Testing/Benchmark/BenchmarkShortcutManager.cs
+++ Assets/Testing/Benchmark/BenchmarkShortcutManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 namespace Benchmarking
@@ -45,16 +46,43 @@ namespace Benchmarking
             DontDestroyOnLoad(s_instance.gameObject);
 
 #if ENABLE_INPUT_SYSTEM
-
+            var action = new InputAction();
+            action.AddCompositeBinding("ButtonWithOneModifier")
+                .With("Button", "<Keyboard>/B")
+                .With("Modifier", "<Keyboard>/leftShift")
+                .With("Modifier", "<Keyboard>/rightShift");
+            action.performed += ctx => StartBenchmark();
+            action.Enable();
 #endif
         }
 
+
 #if ENABLE_INPUT_SYSTEM
+        // TODO: make this an action ?
+        public void Update()
+        {
+            if (SystemInfo.deviceType != DeviceType.Handheld)
+                return;
+
+            if (UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches.Count > 3)
+                StartBenchmark();
+        }
+#endif
+
+#if !ENABLE_INPUT_SYSTEM
         void Update()
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-                if (Input.GetKeyDown(KeyCode.B) && !PerformanceTest.RunningBenchmark)
+            if (SystemInfo.deviceType == DeviceType.Handheld)
+            {
+                if (Input.touchCount > 3 )
                     StartBenchmark();
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                    if (Input.GetKeyDown(KeyCode.B) && !PerformanceTest.RunningBenchmark)
+                        StartBenchmark();
+            }
         }
 #endif
 
